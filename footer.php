aef76dac6a2a535326eec69aa5734a4e453289f8
Otech2018 <ajahogonnaya2012@gmail.com>
2021-01-12 10:35:25 +0100

    homepage

---
+++ footer.php
@@ -0,0 +1,143 @@
+<!-- footer section start-->
+    <div class="footer_main_wrapper index2_footer_wrapper index3_footer_wrapper float_left">
+
+        <div class="container">
+
+            <div class="row">
+
+                <div class="col-lg-4 col-md-6 col-12 col-sm-12">
+                    <div class="wrapper_second_about">
+                      
+                        <p>We are a full service Digital Marketing Agency all the foundational tool you need.</p>
+                        <div class="counter-section">
+                            <div class="ft_about_icon float_left">
+                                <i class="flaticon-user"></i>
+                                <div class="ft_abt_text_wrapper">
+                                    <p class="timer"> 444</p>
+                                    <h4>total member</h4>
+                                </div>
+
+                            </div>
+                            <div class="ft_about_icon float_left">
+                                <i class="flaticon-money-bag"></i>
+                                <div class="ft_abt_text_wrapper">
+                                    <p class="timer">21118.30</p>
+                                    <h4>total deposited</h4>
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-lg-2 col-md-3 col-12 col-sm-4">
+                    <div class="wrapper_second_useful">
+                        <h4> Links </h4>
+
+                        <ul>
+                            
+                            <li><a href="?a=home"><i class="fa fa-angle-right"></i>Home</a>
+                            </li>
+                            
+                            
+                            <li><a href="?a=about"><i class="fa fa-angle-right"></i>About us</a>
+                            </li>
+
+                            <li><a href="?a=plan"><i class="fa fa-angle-right"></i>Plans </a>
+                            </li>
+                            
+                            <li><a href="?a=faq"><i class="fa fa-angle-right"></i>Faq's</a>
+                            </li>
+                        </ul>
+
+                    </div>
+                </div>
+                <div class="col-lg-2 col-md-3 col-12 col-sm-4">
+                    <div class="wrapper_second_useful wrapper_second_links">
+
+                        <ul>
+                            <li><a href="?a=rules"><i class="fa fa-angle-right"></i>Terms</a>
+                            </li>
+                            <li><a href="?a=support"><i class="fa fa-angle-right"></i>Contact Us </a>
+                            </li>
+                            <li><a href="?a=signup"><i class="fa fa-angle-right"></i>Sign Up </a>
+                            </li>
+                            <li><a href="?a=login"><i class="fa fa-angle-right"></i>Log In</a>
+                            </li>
+                            
+                        </ul>
+
+                    </div>
+                </div>
+
+                <div class="col-lg-4 col-md-6 col-12 col-sm-12">
+                    <div class="wrapper_second_useful wrapper_second_useful_2">
+                        <h4>contact  us</h4>
+
+                        <ul>
+                            <li>
+							<a href="tel:+442030263854">
+							<i class="flaticon-phone-contact"></i>
+							<b>	(+44) 203 026 3854 </b> (London Office)
+							</a>
+                            </li>
+							
+							    <li>
+							<a href="tel:+14243513552">
+							<i class="flaticon-phone-contact"></i>
+							<b>	(+1) 424 351 3552 </b> (International)
+							</a>
+                            </li>
+							
+							
+                            <li>
+							<a href="mailto:internationallegalprofit@gmail.com"><i class="flaticon-mail"></i>internationallegalprofit@gmail.com
+							</a>
+                            </li>
+                            <li><a href="https://t.me/iLi2Support"><i class="flaticon-language"></i>Telegram : @iLi2Support</a>
+                            </li>
+
+                            <li><i class="flaticon-placeholder"></i>77 Borough High St, SE1 9GF, UK
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+
+                <div class="col-lg-12 col-md-12 col-xs-12 col-sm-12">
+                    <div class="copyright_wrapper float_left">
+                        <div class="copyright">
+                            <p>Copyright <i class="far fa-copyright"></i> 2020-2021 www.International-Interest.com. all right reserved</p>
+                        </div>
+                        <div class="social_link_foter">
+
+                            <ul>
+                                <li><a href="https://www.facebook.com/InternationalInterest"><i class="fab fa-facebook-f"></i></a></li>
+                                <li><a href="https://twitter.com/Inter4interest"><i class="fab fa-twitter"></i></a></li>
+                                <li><a href="https://www.youtube.com/channel/UCiF3ZeIbh7XoCvi89VKcnZg"><i class="fab fa-youtube"></i></a></li>
+                            	<li><a href="https://www.instagram.com/International_Interest/"><i class="fab fa-instagram"></i></a></li>
+
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+    <!-- footer section end-->
+    <!--custom js files-->
+    <script src="js/jquery-3.3.1.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/modernizr.js"></script>
+    <script src="js/jquery.menu-aim.js"></script>
+    <script src="js/plugin.js"></script>
+    <script src="js/jquery.countTo.js"></script>
+    <script src="js/dropify.min.js"></script>
+    <script src="js/datatables.js"></script>
+    <script src="js/jquery.nice-select.min.js"></script>
+    <script src="js/jquery.inview.min.js"></script>
+    <script src="js/jquery.magnific-popup.js"></script>
+    <script src="js/owl.carousel.js"></script>
+    <script src="js/calculator.js"></script>
+    <script src="js/custom.js"></script>
+    <!-- custom js-->
---
+++ header.php
@@ -0,0 +1,125 @@
+   <!-- preloader Start -->
+    <!-- preloader Start -->
+    <div id="preloader">
+        <div id="status">
+            <img src="images/loader.gif" id="preloader_image" alt="loader">
+        </div>
+    </div>
+    <div class="cursor cursor3"></div>
+    <!-- Top Scroll Start -->
+    <a href="javascript:" id="return-to-top" class="index3_scroll"> <i class="fas fa-angle-double-up"></i></a>
+    <!-- Top Scroll End -->
+    <!-- cp navi wrapper Start -->
+    <nav class="cd-dropdown index3_cd_dropdown d-block d-sm-block d-md-block d-lg-none d-xl-none">
+        <h2><a href="?a=home"> international-interest.com </a></h2>
+        <a href="#0" class="cd-close">Close</a>
+        <ul class="cd-dropdown-content">
+            
+             <li>
+                 
+                <a href="?a=home">Home</a>
+                
+            </li>
+            <li><a href="?a=about"> about us </a></li>
+			<li><a href="?a=news"> news </a></li>			
+            <li><a href="?a=plan"> investment plan </a></li>
+            <li><a href="?a=affiliate"> Affiliate Programme </a></li>
+            <li><a href="?a=faq"> FAQ </a></li>
+            <li>
+                <a href="?a=support">Contact Us</a>
+           
+            </li>
+                        <li>
+                <a href="?a=signup">Sign Up</a></li>
+            <li><a href="?a=login"> login </a></li>
+                    </ul>
+        <!-- .cd-dropdown-content -->
+    </nav>
+    <div class="cp_navi_main_wrapper index2_header_wrapper index3_header_wrapper float_left">
+        <div class="container-fluid">
+            <div class="cp_logo_wrapper">
+                <a href="?a=home">
+                    <img src="images/logo.png" alt="logo">
+                </a>
+            </div>
+            <!-- mobile menu area start -->
+            <header class="mobail_menu d-block d-sm-block d-md-block d-lg-none d-xl-none">
+                <div class="container-fluid">
+                    <div class="row">
+                        <div class="col-lg-12">
+                            <div class="cd-dropdown-wrapper">
+                                <a class="house_toggle" href="#0">
+                                    <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" id="Capa_1" x="0px" y="0px" viewBox="0 0 31.177 31.177" style="enable-background:new 0 0 31.177 31.177;" xml:space="preserve" width="25px" height="25px">
+                                        <g>
+                                            <g>
+                                                <path class="menubar" d="M30.23,1.775H0.946c-0.489,0-0.887-0.398-0.887-0.888S0.457,0,0.946,0H30.23    c0.49,0,0.888,0.398,0.888,0.888S30.72,1.775,30.23,1.775z" fill="#004165" />
+                                            </g>
+                                            <g>
+                                                <path class="menubar" d="M30.23,9.126H12.069c-0.49,0-0.888-0.398-0.888-0.888c0-0.49,0.398-0.888,0.888-0.888H30.23    c0.49,0,0.888,0.397,0.888,0.888C31.118,8.729,30.72,9.126,30.23,9.126z" fill="#004165" />
+                                            </g>
+                                            <g>
+                                                <path class="menubar" d="M30.23,16.477H0.946c-0.489,0-0.887-0.398-0.887-0.888c0-0.49,0.398-0.888,0.887-0.888H30.23    c0.49,0,0.888,0.397,0.888,0.888C31.118,16.079,30.72,16.477,30.23,16.477z" fill="#004165" />
+                                            </g>
+                                            <g>
+                                                <path class="menubar" d="M30.23,23.826H12.069c-0.49,0-0.888-0.396-0.888-0.887c0-0.49,0.398-0.888,0.888-0.888H30.23    c0.49,0,0.888,0.397,0.888,0.888C31.118,23.43,30.72,23.826,30.23,23.826z" fill="#004165" />
+                                            </g>
+                                            <g>
+                                                <path class="menubar" d="M30.23,31.177H0.946c-0.489,0-0.887-0.396-0.887-0.887c0-0.49,0.398-0.888,0.887-0.888H30.23    c0.49,0,0.888,0.398,0.888,0.888C31.118,30.78,30.72,31.177,30.23,31.177z" fill="#004165" />
+                                            </g>
+                                        </g>
+                                    </svg>
+                                </a>
+                                <!-- .cd-dropdown -->
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- .cd-dropdown-wrapper -->
+            </header>
+            <div class="top_header_right_wrapper top_phonecalls">
+                <p><i class="flaticon-phone-contact">
+					</i> (+44) 203 026 3854
+				</p>
+                
+				<div class="header_btn index3_header_btn">
+                    <ul>
+                                                <li>
+                            <a href="?a=signup"> register </a> </li>
+                        <li>
+                            <a href="?a=login"> login </a> </li>
+                                                </ul>
+
+                </div>
+            </div>
+
+            <div class="cp_navigation_wrapper">
+                <div class="mainmenu d-xl-block d-lg-block d-md-none d-sm-none d-none">
+                    <ul class="main_nav_ul">
+                        <li class="has-mega gc_main_navigation"><a href="?a=home" class="gc_main_navigation active_class">Home</a>
+                        </li>   
+                        <li><a href="?a=about" class="gc_main_navigation">about us</a>
+						</li>
+                        <li><a href="?a=news" class="gc_main_navigation">news</a>
+						</li>
+						<li><a href="?a=plan" class="gc_main_navigation">investment plan</a>
+						</li>
+                        
+                        <li class="has-mega gc_main_navigation"><a href="?a=affiliate" class="gc_main_navigation">Affiliate Programe</a>
+                            
+                        </li>
+                        <li class="has-mega gc_main_navigation"><a href="?a=faq" class="gc_main_navigation">FAQs </a>
+                            
+                        </li>
+                        <li class="has-mega gc_main_navigation"><a href="?a=support" class="gc_main_navigation">Contact Us </a>
+                            
+                        </li>
+                       
+                    </ul>
+                </div>
+                <!-- mainmenu end -->
+            </div>
+        </div>
+    </div>
+
+    <!-- navi wrapper End -->
